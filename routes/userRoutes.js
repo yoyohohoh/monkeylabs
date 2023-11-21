cc343e63292f2cc6ee2be4db408c224e7f6a974c
yoyohohoh <113647176+yoyohohoh@users.noreply.github.com>
2023-11-20 19:42:59 -0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/api/users', userController.getAllUsers);
 router.get('/api/users/:id', userController.getUserById);
-router.get('/api/users/:username', userController.getUserByUsername);
+//router.get('/api/users/:username', userController.getUserByUsername);
 
 router.post('/api/users', userController.createUser);
 
